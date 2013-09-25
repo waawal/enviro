a806591a88a9408b29dacd965a7f8fb714de5fc1
Daniel Waardal <waawal@boom.ws>
2013-09-25 17:59:59 +0200

    implementing Env and ConfigFileWrapper

---
+++ friendlyenv.py
@@ -0,0 +1,45 @@
+from __future__ import generators
+
+try:
+    import ConfigParser as configparser
+    PY2 = True
+except ImportError:
+    import configparser
+    PY2 = False
+
+class ConfigFileWrapper(object):
+
+    def __init__(self, filename):
+        self.fp = open(filename)
+        self.section = '[default-section]\n'
+
+   def readline(self):
+        if self.section:
+            try:
+                return self.section
+            finally:
+                self.section = None
+        else:
+            return self.fp.readline()
+
+    def __iter__(self):
+        for line in self.readline:
+            yield line
+
+class Env(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def find_file(self):
+        self.filename
+        self.file = ConfigFileWrapper(self.filename)
+
+    def parse_file(self):
+        config = configparser.ConfigParser()
+        if PY2:
+            config.readfp(self.file, self.filename)
+        else:
+            config.read_file(self.file, self.filename)
+
+        self.config = dict(config)
