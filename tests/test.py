081193d349b509db38d401a555921f4df121931a
Daniel Waardal <waawal@boom.ws>
2013-10-10 11:14:15 +0200

    adding initial tests for file resolving

--- tests/test.py
+++ tests/test.py
@@ -7,22 +7,77 @@ test_enviro
 
 Tests for `enviro` module.
 """
+import sys
 
+sys.path.append('../') # Yeah... well... The price you have to pay.
+
+import os
+import shutil
 import unittest
+import __main__
 
-from enviro import env
+import enviro
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+FIXTURE = os.path.join(TEST_DIR, 'fixtures', 'mysettings.conf')
 
 
-class TestElastorm(unittest.TestCase):
+class TestCWD(unittest.TestCase):
 
     def setUp(self):
-        pass
+        shutil.copyfile(FIXTURE, os.path.join(os.getcwd(), 'mysettings.conf'))
 
     def test_something(self):
-        pass
+        enviro.setdefault('mysettings.conf')
+        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+
 
     def tearDown(self):
-        pass
+        os.remove(os.path.join(os.getcwd(), 'mysettings.conf'))
+
+
+class TestHome(unittest.TestCase):
+
+    def setUp(self):
+        shutil.copyfile(FIXTURE, os.path.join(os.path.expanduser('~'), 'mysettings.conf'))
+
+    def test_something(self):
+        enviro.setdefault('mysettings.conf')
+        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+
+
+    def tearDown(self):
+        os.remove(os.path.join(os.path.expanduser('~'), 'mysettings.conf'))
+
+class TestETC(unittest.TestCase):
+
+    def setUp(self):
+        shutil.copyfile(FIXTURE, os.path.join('/etc', 'mysettings.conf'))
+
+    def test_something(self):
+        enviro.setdefault('mysettings.conf')
+        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+
+
+    def tearDown(self):
+        os.remove(os.path.join('/etc', 'mysettings.conf'))
+
+
+class TestScriptPath(unittest.TestCase):
+
+    def setUp(self):
+        shutil.copyfile(FIXTURE, os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
+                                                              'mysettings.conf'))
+
+    def test_something(self):
+        enviro.setdefault('mysettings.conf')
+        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+
+
+    def tearDown(self):
+        os.remove(os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
+                                                              'mysettings.conf'))
+
 
 if __name__ == '__main__':
     unittest.main()
