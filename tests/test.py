faafc92b9e4e72b380eb71f4386a6a6c83ed2246
Daniel Waardal <waawal@boom.ws>
2013-10-10 16:56:50 +0200

    testing should work with forking

--- tests/test.py
+++ tests/test.py
@@ -19,64 +19,88 @@ import __main__
 import enviro
 
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
-FIXTURE = os.path.join(TEST_DIR, 'fixtures', 'mysettings.conf')
+SETTINGS1 = os.path.join(TEST_DIR, 'fixtures', 'mysettings.conf')
+SETTINGS2 = os.path.join(TEST_DIR, 'fixtures', 'myothersettings.conf')
+print SETTINGS1, SETTINGS2
 
 
 class TestCWD(unittest.TestCase):
 
     def setUp(self):
-        shutil.copyfile(FIXTURE, os.path.join(os.getcwd(), 'mysettings.conf'))
+        shutil.copyfile(SETTINGS1, os.path.join(os.getcwd(), 'mysettings.conf'))
 
     def test_something(self):
         enviro.setdefault('mysettings.conf')
-        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+        self.failUnlessEqual(os.environ['foodir'], 'frob/whatever')
 
 
     def tearDown(self):
         os.remove(os.path.join(os.getcwd(), 'mysettings.conf'))
+        os.environ = {}
 
 
 class TestHome(unittest.TestCase):
 
     def setUp(self):
-        shutil.copyfile(FIXTURE, os.path.join(os.path.expanduser('~'), 'mysettings.conf'))
+        shutil.copyfile(SETTINGS1, os.path.join(os.path.expanduser('~'),
+                                              'mysettings.conf'))
 
     def test_something(self):
         enviro.setdefault('mysettings.conf')
-        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
+        self.failUnlessEqual(os.environ['foodir'], 'frob/whatever')
 
 
     def tearDown(self):
         os.remove(os.path.join(os.path.expanduser('~'), 'mysettings.conf'))
+        os.environ = {}
 
 class TestETC(unittest.TestCase):
 
     def setUp(self):
-        shutil.copyfile(FIXTURE, os.path.join('/etc', 'mysettings.conf'))
+        shutil.copyfile(SETTINGS1, os.path.join('/etc', 'mysettings.conf'))
 
     def test_something(self):
         enviro.setdefault('mysettings.conf')
-        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
-
+        self.failUnlessEqual(os.environ['foodir'], 'frob/whatever')
 
     def tearDown(self):
         os.remove(os.path.join('/etc', 'mysettings.conf'))
+        os.environ = {}
 
 
 class TestScriptPath(unittest.TestCase):
 
     def setUp(self):
-        shutil.copyfile(FIXTURE, os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
-                                                              'mysettings.conf'))
+        shutil.copyfile(SETTINGS1, os.path.join(
+            os.path.dirname(os.path.realpath(__main__.__file__)),
+            'mysettings.conf'))
 
     def test_something(self):
         enviro.setdefault('mysettings.conf')
-        self.failUnlessEqual(os.environ.get('foodir'), 'frob/whatever')
-
+        self.failUnlessEqual(os.environ['foodir'], 'frob/whatever')
 
     def tearDown(self):
-        os.remove(os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
-                                                              'mysettings.conf'))
+        os.remove(os.path.join(
+            os.path.dirname(os.path.realpath(__main__.__file__)),
+            'mysettings.conf'))
+        os.environ = {}
+
+
+class TestOverrides(unittest.TestCase):
+
+    def setUp(self):
+        shutil.copyfile(SETTINGS1, os.path.join('/etc', 'mysettings.conf'))
+        shutil.copyfile(SETTINGS2, os.path.join(os.getcwd(), 'mysettings.conf'))
+
+    def test_something(self):
+        print "now!"
+        enviro.setdefault('mysettings.conf')
+        self.failUnlessEqual(os.environ['foodir'], 'yolo/whatever')
+
+    def tearDown(self):
+        os.remove(os.path.join('/etc', 'mysettings.conf'))
+        os.remove(os.path.join(os.getcwd(), 'mysettings.conf'))
+        os.environ = {}
 
 
 if __name__ == '__main__':
